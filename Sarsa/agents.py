4cd78bd7ba617b50c4b83b47423e7b9861333d66
FredAmouzgar <realfred.amouzgar@gmail.com>
2020-10-14 07:02:31 +1100

    Deep RL agent added

--- Sarsa/agents.py
+++ Sarsa/agents.py
@@ -1,4 +1,7 @@
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_probability as tfp
 
 class Sarsa_Agent:
     def __init__(self, states_n, actions_n, alpha=0.5, epsilon=0.1, gamma=0.95, epsilon_decay=True, epsilon_decay_factor=0.01):
@@ -31,3 +34,36 @@ class Sarsa_Agent:
         if done and self.epsilon_decay:
             self.decay_epsilon(self.epsilon_decay_factor)
         return self.new_a
+
+class Deep_Agent:
+    def __init__(self, state_size, action_size, env_name, gamma=0.99, alpha_w=0.15, alpha_theta=0.1):
+        self.I = 1
+        self.state_size = state_size
+        self.action_size = action_size
+        self.env_name = env_name
+        self.gamma = gamma
+        self.alpha_w = alpha_w
+        self.alpha_theta = alpha_theta
+
+        self.actor = keras.Sequential([keras.layers.Dense(50, activation="relu", input_shape=(self.state_size,)),
+                                       keras.layers.Dense(action_size, activation=keras.activations.sigmoid)])
+
+
+    def new_episode(self):
+        self.I = 1
+
+    def act(self, state):
+        # Returns action from policy
+        state = self.process_state(state)
+        logits = self.actor(state)
+        action_probs = tfp.distributions.Categorical(probs=logits)
+        action = action_probs.sample()
+
+        return action.numpy()[0]
+
+
+    def process_state(self, state):
+        return tf.convert_to_tensor(state.reshape(1, self.state_size), dtype=tf.float32)
+        
+    def load(self):
+        self.actor = tf.keras.models.load_model(f"actor_{self.env_name}.h5")

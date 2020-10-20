e94d63284ac8c15d0e70ec19a46dc7825e46377f
FredAmouzgar <realfred.amouzgar@gmail.com>
2020-10-20 22:06:34 +1100

    Sarsa save and load feature added

--- Sarsa/agents.py
+++ Sarsa/agents.py
@@ -2,9 +2,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow_probability as tfp
+import pickle
+
 
 class Sarsa_Agent:
-    def __init__(self, states_n, actions_n, alpha=0.5, epsilon=0.1, gamma=0.95, epsilon_decay=True, epsilon_decay_factor=0.01):
+    def __init__(self, states_n, actions_n, alpha=0.5, epsilon=0.1, gamma=0.95, epsilon_decay=True,
+                 epsilon_decay_factor=0.01):
         self.alpha = alpha
         self.epsilon = epsilon
         self.gamma = gamma
@@ -14,26 +17,35 @@ class Sarsa_Agent:
         self.new_a = None
         self.epsilon_decay = epsilon_decay
         self.epsilon_decay_factor = epsilon_decay_factor
-    
+
     def act(self, state):
         # epsilon greedy
         if np.random.rand() < self.epsilon:
             act = np.random.choice(np.arange(self.actions_n))
         else:
-            act = np.argmax(self.Q[int(state),:])
+            act = np.argmax(self.Q[int(state), :])
         return act
-    
+
     def decay_epsilon(self, factor):
-        self.epsilon -= factor if self.epsilon >=0 else 0
-    
+        self.epsilon -= factor if self.epsilon >= 0 else 0
+
     def update(self, new_s, r, s, a, done):
         self.new_a = self.act(new_s)
         mask = 0 if done else 1
         s, a, self.new_a, new_s = int(s), int(a), int(self.new_a), int(new_s)
-        self.Q[s,a] += self.alpha * (r + self.gamma * self.Q[new_s, self.new_a] * mask - self.Q[s,a])
+        self.Q[s, a] += self.alpha * (r + self.gamma * self.Q[new_s, self.new_a] * mask - self.Q[s, a])
         if done and self.epsilon_decay:
             self.decay_epsilon(self.epsilon_decay_factor)
         return self.new_a
+
+    def save(self, file_name="taxi.pkl"):
+        with open(file_name, mode="wb") as f:
+            pickle.dump(self.Q, f)
+
+    def load(self, file_name="taxi.pkl"):
+        with open(file_name, mode="rb") as f:
+            self.Q = pickle.load(f)
+
 
 class Deep_Agent:
     def __init__(self, state_size, action_size, env_name, gamma=0.99, alpha_w=0.15, alpha_theta=0.1):
@@ -48,7 +60,6 @@ class Deep_Agent:
         self.actor = keras.Sequential([keras.layers.Dense(50, activation="relu", input_shape=(self.state_size,)),
                                        keras.layers.Dense(action_size, activation=keras.activations.sigmoid)])
 
-
     def new_episode(self):
         self.I = 1
 
@@ -61,10 +72,9 @@ class Deep_Agent:
 
         return action.numpy()[0]
 
-
     def process_state(self, state):
         return tf.convert_to_tensor(state.reshape(1, self.state_size), dtype=tf.float32)
-        
+
     def load(self, src="brain_CartPole-v1.h5"):
-        #self.actor = tf.keras.models.load_model(src)
+        # self.actor = tf.keras.models.load_model(src)
         self.actor.load_weights(src)
